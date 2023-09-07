9dc87f04428fad75f5409f00758196722254ecd3
RubenKraan <125913576+RubenKraan@users.noreply.github.com>
2023-09-07 12:59:17 +0200

    first commit

---
+++ FrontEnd/login/login.js
@@ -0,0 +1,51 @@
+const loginUrl = "http://localhost:5678/api/users/login";
+const inputEmail = document.getElementById("email");
+const inputPassword = document.getElementById("password");
+const submitButton = document.querySelector("imput[type='submit']");
+const form = document.getElementById("loginForm");
+const loginError =  document.querySelector(".loginError");
+const passwordError = document.querySelector(".passwordError");
+
+const logUser = {
+    email: "",
+    password: "",
+};
+
+async function loginUser() {
+    try{
+        await fetch(loginUrl, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+
+          } , body: JSON.stringify(logUser), 
+        })
+        .then((response) => response.json())
+      .then((responseData) => {
+        data = responseData;
+        console.log(data);
+      });
+      if (data.message){
+        loginError.textContent = "Erreur dans l’identifiant !!";
+        inputEmail.style.color = "red";
+        console.log(logUser);
+      } else if (data.error) {
+        passwordError.textContent = "Erreur dans le mot de passe !!";
+        loginError.textContent = "";
+        inputPassword.style.color = "red";
+        inputEmail.style.color = "#1d6154";
+  
+        console.log(logUser);
+      } else {
+        inputPassword.style.color = "#1d6154";
+        passwordError.textContent = "";
+        loginError.textContent = "";
+        console.log("LogAdmin OK");
+        console.log(logUser);
+        localStorage.setItem("token", data.token);
+        window.location.href = "../index.html";
+      }
+    } catch (error) {
+      console.log(error);
+    }
+}
